2406c4018079524d87b4a7a2c211465ca57a5b32
sungmin Hong <johnnyhong@sungmins-MacBook-Air.local>
2023-12-14 13:09:44 +0900

    Feat: wedding 데이터 호출기능 추가

--- online_invitaion/src/App.tsx
+++ online_invitaion/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classNames from 'classnames/bind'
 
 import styles from './App.module.scss'
@@ -6,7 +6,22 @@ import styles from './App.module.scss'
 const cx = classNames.bind(styles)
 
 function App() {
-  return <div className={cx('container')}></div>
+  // wedding 데이터 useState
+  const [wedding, setWedding] = useState(null)
+
+  // 1. wedding 데이터 호출 ([]가 비어있기 마운트 될때만 실행 (처음만실행))
+  useEffect(() => {
+    // callback, promise, async/await
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        // wedding state에 값넣어주기
+        setWedding(data)
+      })
+  }, [])
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
