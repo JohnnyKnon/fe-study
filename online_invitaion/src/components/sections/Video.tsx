2c1e29e4620ddca8aa10078a515a8eb536f71ada
sungmin Hong <johnyhong24@gmail.com>
2023-12-14 15:53:22 +0900

    add: video 태그를 통하여 인트로 동영상 추가

--- online_invitaion/src/components/sections/Video.tsx
+++ online_invitaion/src/components/sections/Video.tsx
@@ -7,7 +7,20 @@ import Section from '@shared/Section'
 const cx = classNames.bind(styles)
 
 function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      {/* video 태그 내부에 source가 하나 이상일 경우 autoPlay를 위해서 muted가 필수로 필요 */}
+      {/* 또한, 직접 플레이 조종이나, 크게보기등 유튜브 같은 기능을 원하면 controls={true} 필요 */}
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" />
+      </video>
+    </Section>
+  )
 }
 
 export default Video
