bd75f844256c5d5f174073a2e294e487aa1af50d
sungmin Hong <johnyhong24@gmail.com>
2023-12-15 10:59:42 +0900

    style: 이미지 갤러리 격자형태로 스타일 추가

--- online_invitaion/src/components/sections/ImageGallery.tsx
+++ online_invitaion/src/components/sections/ImageGallery.tsx
@@ -7,10 +7,10 @@ const cx = classNames.bind(styles)
 function ImageGallery({ images }: { images: string[] }) {
   return (
     <Section title="사진첩">
-      <ul>
+      <ul className={cx('wrap-images')}>
         {/* index를 사용하는건 좋지 않지만 유동적으로 변경되는 값이 아니기에 일단 이렇게 처리 */}
         {images.map((src, idx) => (
-          <li key={idx}>
+          <li key={idx} className={cx('wrap-image')}>
             <img src={src} alt="사진첩 이미지" />
           </li>
         ))}
