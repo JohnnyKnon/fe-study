9a3b54d0b08f0626fadb01cc23113a06c8559779
sungmin Hong <johnyhong24@gmail.com>
2023-12-15 12:00:05 +0900

    Feat: 이미지 슬라이더 on/off기능 추가 및 이미지 슬라이더 구현과 상단 스타일 마진값 추가/수정

---
+++ online_invitaion/src/components/sections/Video 2.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames/bind'
+import styles from './Video.module.scss'
+
+// 공통 섹션 디자인 불러오기
+import Section from '@shared/Section'
+
+const cx = classNames.bind(styles)
+
+function Video() {
+  return <Section>Video</Section>
+}
+
+export default Video
